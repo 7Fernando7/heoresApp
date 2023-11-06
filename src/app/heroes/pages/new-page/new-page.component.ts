3684d5370db1c979b63ce71b6c43d7da59587320
Pereira De Paiva <fernando.pereira@transfesa.com>
2023-11-06 14:07:53 +0100

    Conectar el formulario con el template

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -10,11 +11,12 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class NewPageComponent {
 
   public heroForm = new FormGroup({
-    id:               new FormControl(''),
+
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', { nonNullable: true}),
+    publisher:        new FormControl<Publisher>(Publisher.DCComics),
     alter_ego:        new FormControl(''),
     first_appearance: new FormControl(''),
-    publisher:        new FormControl(''),
-    superhero:        new FormControl(''),
     characters:       new FormControl(''),
     alt_img:          new FormControl(''),
   });
@@ -23,5 +25,13 @@ export class NewPageComponent {
     { id: 'DC Comics', desc: 'DC - Comics'},
     { id: 'Marvel Comics', desc: 'Marvel - Comics'},
   ];
+
+  onSubmit(): void {
+
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+     })
+  }
 
 }
